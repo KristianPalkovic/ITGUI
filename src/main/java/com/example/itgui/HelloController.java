fb7ecb907c5ea1e99d6826aafcd4e8f8c2793fc4
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2023-03-23 11:18:53 +0100

    commit

--- src/main/java/com/example/itgui/HelloController.java
+++ src/main/java/com/example/itgui/HelloController.java
@@ -2,13 +2,29 @@ package com.example.itgui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private Label menoPriezvisko;
+
+    @FXML
+    private TextField prveTextovePole;
+
+    public void initialize(){
+        System.out.println(menoPriezvisko.getText());
+    }
+
+    @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        welcomeText.setText("Toto je moja GUI Aplikácia");
+    }
+
+    @FXML
+    protected void zobrazPole() {
+        System.out.println("test tlačidla");
     }
 }
