8e8a7a6614cb1db169b29a3254c0f66cc023e724
KristianPalkovic <Kristian.Palkovic@sosholic.sk>
2023-04-13 12:46:04 +0200

    commit

--- src/main/java/com/example/itgui/gridpane/PrvyGridPaneController.java
+++ src/main/java/com/example/itgui/gridpane/PrvyGridPaneController.java
@@ -1,4 +1,35 @@
 package com.example.itgui.gridpane;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrvyGridPaneController {
+    @FXML
+    private Label loginText;
+
+    @FXML
+    private TextField pole;
+
+    @FXML
+    private PasswordField password;
+
+
+    private Map<String,String> passwordStore = new HashMap<>();
+    {
+        passwordStore.put("john","123");
+        passwordStore.put("marry","456");
+    }
+    @FXML
+    public void process(){
+        if (){
+            System.out.println(passwordStore.get("john"));
+        }else{
+            System.out.println("Neplatné zadanie mena alebo hesla");
+        }
+    }
 }
